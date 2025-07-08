70bf97df6b1d1b35fae073bec8717879311fb882
MasahikoTerazono <pink-panther.m-y.1-8@docomo.ne.jp>
2025-07-09 00:24:50 +0900

    APIキー取得処理を追加

--- app.py
+++ app.py
@@ -19,27 +19,4 @@ if selected_item == "文字数カウント":
     text_count = len(input_message)
 
 else:
-    height = st.text_input(label="身長（cm）を入力してください。")
-    weight = st.text_input(label="体重（kg）を入力してください。")
-
-if st.button("実行"):
-    st.divider()
-
-    if selected_item == "文字数カウント":
-        if input_message:
-            st.write(f"文字数: **{text_count}**")
-
-        else:
-            st.error("カウント対象となるテキストを入力してから「実行」ボタンを押してください。")
-
-    else:
-        if height and weight:
-            try:
-                bmi = round(int(weight) / ((int(height)/100) ** 2), 1)
-                st.write(f"BMI値: {bmi}")
-
-            except ValueError as e:
-                st.error("身長と体重は数値で入力してください。")
-
-        else:
-            st.error("身長と体重をどちらも入力してください。")
+    st.error("❌ APIキーが読み込まれていません")
